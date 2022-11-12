11a8105ac125e24ceaac24b993acab473b4b579a
aldok <aldo.santos.oliveira95@gmail.com>
2022-11-11 23:47:15 -0300

    Initial commit

---
+++ src/main/java/com/example/dioswagger/DioSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dioswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DioSwaggerApplication.class, args);
+    }
+
+}
